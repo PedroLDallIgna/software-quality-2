a20a7fb8b04314a2bc8b799eddf68dec16303e22
Henrique Bonatto <nattohen@gmail.com>
2024-04-17 10:28:58 -0300

    atualização lista de pedidos

--- src/index.js
+++ src/index.js
@@ -3,3 +3,22 @@ const { checkOrder } = require("./utils/checkOrder.js");
 console.log('Running');
 console.log(checkOrder(false, 100));
 console.log(checkOrder(true, 100));
+
+const pedido = document.querySelector(".pedidos");
+const nome = document.querySelector("#name");
+const valor = document.querySelector("#value");
+const confirm = document.querySelector("#submit");
+
+const check = document.querySelector("#first-buy");
+
+confirm.addEventListener("click", () => {
+  if(check.checked) {
+    createPedido(nome.value, checkOrder(true, valor.value));
+  } else {
+    createPedido(nome.value, checkOrder(false, valor.value));
+  }
+})
+
+function createPedido(nome, valor) {
+  pedido.innerHTML += `<p>${nome}: ${valor}</p>`;
+}
--- src/utils/checkOrder.js
+++ src/utils/checkOrder.js
@@ -3,12 +3,12 @@ const { calcNewValue } = require("./calcNewValue.js");
 function checkOrder(first, value) {
     
     if (first) {
-        return calcNewValue(value, -15);
+        return calcNewValue(value, -15)
     } else {
         if (value > 200 && value <= 500) {
             return calcNewValue(value, -5)
         } else if (value > 500) {
-            return calcNewValue(value, -10);
+            return calcNewValue(value, -10)
         }
     }
 
